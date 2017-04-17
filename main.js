70e3ba1116c000782f992a31b83f3277067d01b9
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-04-17 19:07:53 -0400

    Some Basic Setup

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 canvas = document.getElementById("mainCanvas");
 ctx = canvas.getContext("2d");
 ctx.fillRect(1,1,50,50);
+for(var i = 0;i<100;i++){
+	for(var j = 0;j<100;j++){
+		ctx.fillStyle = "rgb("+i+",127,"+j+")";
+		ctx.fillRect(i,j,50,50);
+	}
+}
