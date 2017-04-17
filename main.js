340356dd4325caa55440a6f5764ba202305b807c
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-04-17 19:16:01 -0400

    A Change of Plans

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ ctx.fillRect(1,1,50,50);
 for(var i = 0;i<255;i++){
 	for(var j = 0;j<255;j++){
 		ctx.fillStyle = "rgb("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+")";
-		ctx.fillRect(i,j,50,50);
+		ctx.fillRect(i*5,j*5,5,5);
 	}
 }
