a2fc7f769f9d6d64a51f07a1cfdcc7daddad03bc
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-06-01 08:36:16 -0400

    basic stuff slapped on

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 
 
 
+/*
+===REMEMBER===
+
+ > BE ORGANIZED
+
+ > DONT SETTLE FOR HALF-BAKED CODE
+
+ > FUNCTIONALIZE EVERYTHING
+
+===REMEMBER==
+*/
+
 //SETUP VARIABLES / GLOBALS
 //======================================================//
 canvas = document.getElementById("gameCanvas");
@@ -9,7 +21,7 @@ ctx = canvas.getContext('2d');
 
 //COMMON FUNCTIONS
 //======================================================//
-function drawTile(imgName,x,y){
+function drawTile(imgName,x,y){ //JUST DRAWS, DOES NOT SET ANYTHING AND CAN BE FORCED TO DRAW NON-EXISTANT TILES
 	img = document.getElementById(imgName);
 	ctx.drawImage(img,x*32,y*32); //USES GRID COORDINATES, NOT TRUE COORDINATES
 }
@@ -20,11 +32,6 @@ function setTile(x,y,grid,blockName){ // proper useage: grid = setTile(x,y,grid,
 	return grid;
 }
 //======================================================//
-
-/* COPIED FROM ORIGINAL (DELETE LATER)
-img = document.getElementById(grid[i][j]["entityContent"]);
-						ctx.drawImage(img,i*32,j*32);
-*/
 
 //UNCOMMON FUNCTIONS
 //======================================================//
@@ -37,4 +44,41 @@ function drawGrid(grid,width,height,xOffset,yOffset){ //OFFSETS DONT WORK, PLEAS
 	}
 }
 
+//======================================================//
+
+
+//INVENTORY DISPLAY STUFF (HALF BAKED VERSION)
+//======================================================//
+/*
+function inventoryDisplay(slot){
+	drawHalt = true;
+	if(slot == null){
+		for(var i = 0; i<20;i++){
+			if(inventory[i] != null){
+				displayItem(i+player.x,player.y,inventory[i]["name"],inventory[i]["amount"],player.selectedItem == i);
+			}
+		}
+	}else{
+		if(inventory[slot]["name"] != null){
+			displayItem(player.x,player.y,inventory[slot]["name"],inventory[slot]["amount"],player.selectedItem == slot);
+		}
+	}
+}
+	
+function displayItem(x,y,name,amount,selected){
+	img = document.getElementById(name);
+	ctx.fillStyle = "rgb(200,200,200)";
+	ctx.font = "bold 14px Courier";
+	ctx.fillRect((x*32)+6,((y*32)-32)+6,20,20);
+	if(selected != false){
+		ctx.fillStyle = "rgb(255,255,255)";
+		ctx.fillRect((x*32)+4,((y*32)-32)+4,24,24);
+	}
+	ctx.drawImage(img,(x*32)+8,((y*32)-32)+8,16,16);
+	if(amount != null){
+		ctx.fillStyle = "black";
+		ctx.fillText(amount,(x*32)+20,(y*32-4));
+	}
+}
+*/
 //======================================================//
